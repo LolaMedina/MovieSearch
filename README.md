652375848859758612a81fb61c5a8e75c27b3942
LolaMedina <mlola.oseni@gmail.com>
2021-04-26 10:57:44 +0100

    .

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # MovieSearch
-A simple movie search app. This is a task under SCA Mentorship Program C4
+A simple movie search app written in vanilla JS, HTML and CSS. 
+
+This is a task under SCA Mentorship Program C4
---
+++ src/script.js
@@ -0,0 +1,38 @@
+// API details
+const apiUrl = 'https://api.themoviedb.org/3/movie/popular?api_key=f3f08c027c288ebf1a09b168451ce353&language=en-US&page=1';
+const img_path = 'https://image.tmdb.org/t/p/w500//';
+
+// Select elements from html file
+const main = document.querySelector('#main');
+
+const truncate = (str,num) => {
+    if(str.length <= num){
+    return str
+}
+   return str.slice(0,num) + '...'
+}
+
+//Calling the movie API
+let displayMovies = url =>{
+    fetch(url).then(res => res.json())
+    .then(function(data){
+        data.results.forEach(element =>{
+            const div = document.createElement('div');
+            const image = document.createElement('img');
+            const movieTitle= document.createElement('h2');
+            const text = document.createElement('p');
+            
+            movieTitle.innerHTML = `${element.title}`;
+            text.innerHTML = truncate(`${element.overview}`, 40);
+            image.src = img_path + element.poster_path;
+            div.appendChild(image);
+            div.appendChild(movieTitle);
+            div.appendChild(text);
+            main.appendChild(div);
+        });
+    });
+}
+
+
+
+displayMovies(apiUrl);
